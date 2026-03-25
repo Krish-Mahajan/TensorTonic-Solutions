c4077b4e6a5c6481cf73e74dff00cd9e09376bd1
Krishna Mahajan <krish.mahajan@gmail.com>
2026-03-24 23:30:49 -0700

    Add solution for transformers-tokenization

---
+++ transformer/transformers-tokenization/transformers-tokenization.py
@@ -0,0 +1,55 @@
+import numpy as np
+from typing import List, Dict
+
+class SimpleTokenizer:
+    """
+    A word-level tokenizer with special tokens.
+    """
+    
+    def __init__(self):
+        self.word_to_id: Dict[str, int] = {}
+        self.id_to_word: Dict[int, str] = {}
+        self.vocab_size = 0
+        
+        # Special tokens
+        self.pad_token = "<PAD>"
+        self.unk_token = "<UNK>"
+        self.bos_token = "<BOS>"
+        self.eos_token = "<EOS>"
+    
+    def build_vocab(self, texts: List[str]) -> None:
+        """
+        Build vocabulary from a list of texts.
+        Add special tokens first, then unique words.
+        """
+        special_tokens = [self.pad_token, self.unk_token, self.bos_token, self.eos_token]
+        for token in special_tokens:
+            self.word_to_id[token] = len(self.word_to_id)
+            self.id_to_word[self.word_to_id[token]] = token
+
+
+        
+        for text in texts:
+            for word in text.split():
+                if word not in self.word_to_id:
+                    idx = len(self.word_to_id)
+                    self.word_to_id[word] = idx 
+                    self.id_to_word[idx] = word 
+        
+        self.vocab_size = len(self.word_to_id)
+   
+    
+    def encode(self, text: str) -> List[int]:
+        """
+        Convert text to list of token IDs.
+        Use UNK for unknown words.
+        """
+        unk_id = self.word_to_id[self.unk_token]
+        return [ self.word_to_id.get(word, unk_id)  for word in text.split() ]
+    
+    
+    def decode(self, ids: List[int]) -> str:
+        """
+        Convert list of token IDs back to text.
+        """
+        return " ".join(self.id_to_word.get(id, self.unk_token) for id in ids)
